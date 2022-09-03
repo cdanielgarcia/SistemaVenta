ef1e91de75655b5e42ae811b108ec291aecfa7b0
nmedinaes <nicolasmeddina789@gmail.com>
2022-09-03 11:20:38 -0500

    Migración Estados

---
+++ Migrations/20220903161813_Dos.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaVenta.Migrations
+{
+    public partial class Dos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Usuarios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Proveedores",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Productos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Clientes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estado",
+                table: "Categorias",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Proveedores");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Productos");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Categorias");
+        }
+    }
+}
